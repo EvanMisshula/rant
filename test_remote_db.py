aadea91ecf6f9003b50873f462c084eda065f092
Evan Misshula <emisshula@jjay.cuny.edu>
2015-06-26 23:27:19 -0400

    eliminated header in RantSpider

--- test_remote_db.py
+++ test_remote_db.py
@@ -16,14 +16,15 @@ else:
     os.chdir('/Users/emisshula/Documents/insight/lscrape')
     f1 = open("/Users/emisshula/Documents/insight/lscrape/db_settings.csv",'r')
 
+    
 sql_params = f1.read()
 f1.close()
 
 db = sqlalchemy.create_engine(sql_params)
 conn = db.connect()
 
-mySpider = Rs.RantSpider("http://theerant.yuku.com/forums/58/THEE-RANT/THEE-RANT", headers = [])
-response = mySpider.get_response()
+mySpider = Rs.RantSpider("http://theerant.yuku.com/forums/58/THEE-RANT/THEE-RANT")
+#response = mySpider.get_response()
 headers= response.headers
 mySpider.doc = mySpider.get_parsed_doc()
 
