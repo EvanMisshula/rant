d2ef409622a7bbcac5c210f8fe4f166e4dfdbe45
Evan Misshula <emisshula@jjay.cuny.edu>
2015-06-29 11:23:16 -0400

    corrected print statements

--- get_comments_remote.py
+++ get_comments_remote.py
@@ -33,10 +33,10 @@ topic_titles = df1['Topic'].values.tolist()
 for topic_idx,topic_val in enumerate(topic_head_link):
     print("topic_idx: %d" % topic_idx)
     next_url_list = [topic_head_link[topic_idx]]
-    print("this is the topic title: %s" % topic_titles[topic_idx])
+    print("This is the topic title: %s" % topic_titles[topic_idx])
     pages_on_this_topic = 0
     while next_url_list != []:
-        print("The url to get is: %s" % next_url_list[0])
+        print("The url to get is:\n %s" % next_url_list[0])
         forumSpider = Rs.RantSpider(next_url_list[0])
 #        response = forumSpider.get_response()
         forumSpider.doc = forumSpider.get_parsed_doc()
@@ -46,7 +46,7 @@ for topic_idx,topic_val in enumerate(topic_head_link):
             post_time=[unicode(r.text) for r in forumSpider.doc.xpath('.//span[@class="date"]')]
         post_comments = [ unicode(re.sub('[\\n\\t\']','',s.text_content())) for s in forumSpider.doc.xpath('.//div[starts-with(@class,"post-body")]/div[@class="scrolling"]/div')]
         doc_title = [ topic_titles[topic_idx] ] * len(post_comments)
-        print("collected: %d pages of data on this topic." % (pages_on_this_topic+1))
+        print("Collected: %d pages of data on this topic." % (pages_on_this_topic+1))
         # get post date
         next_url_list = forumSpider.doc.xpath('.//div[@class="next active"]/a')
         if next_url_list != []:
@@ -57,13 +57,15 @@ for topic_idx,topic_val in enumerate(topic_head_link):
         d = { 'authors' : pd.Series(post_authors, index=range(len(post_time))), 'comments' : pd.Series(post_comments, index=range(len(post_time))), 'post_time' : pd.Series(post_time, index=range(len(post_time))), 'document' : pd.Series(doc_title, index=range(len(doc_title)))}
         df=pd.DataFrame(d)
         print("There are %d rows in the dataframe we are appending" % len(df.index))
-        print("There are %d rows in the dataframe scraped so far." % len(df1.index))
         print("There are %d columns in the dataframe we are appending." % len(df.columns))
-        print("There are %d columns in the dataframe we scraped so far." % len(df1.columns))
+
         if topic_idx==0 or pages_on_this_topic==0:
+            print('This is the first page of comments')
             df.to_sql(name='comments',con=db,schema=None,if_exists='replace',index=True,index_label=None)
             df1=df
         else:
+            print("There are %d rows in the dataframe scraped so far." % len(df1.index))
+            print("There are %d columns in the dataframe we scraped so far." % len(df1.columns))
             df1=pd.concat([df1,df])
             df1.to_sql(name='comments',con=db,schema=None,if_exists='replace',index=True,index_label=None)
         print("saved df1 with %d records." % len(df1.index))
